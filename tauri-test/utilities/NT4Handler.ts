edc8128162dfb1f0b413004fa52c35e9910f5562
Autumn <62668093+Autumn-Ou@users.noreply.github.com>
2023-06-10 20:09:35 -0400

    docs

--- tauri-test/utilities/NT4Handler.ts
+++ tauri-test/utilities/NT4Handler.ts
@@ -15,7 +15,15 @@ function StartNetworkTableHandler(address: number[], port: number): void {
         .catch(console.error)
 }
 
-// TODO: backend must be implemented
+/**
+ * Ends a network table client connected to the specified address and port
+ * @param address an array of 4 numbers representing the ipv4 address of the server
+ * formatted as [0-255, 0-255, 0-255, 0-255] and interpreted as [a, b, c, d] -> a.b.c.d
+ * @param port a number representing the port of the server, must be between 0 and 65535
+ *
+ * This function calls on the native backend and may result in a crash.
+ * TODO: backend must be implemented
+ */
 function StopNetworkTableHandler(address: number[], port: number): void {
     invoke('stop_network_table_handler', { address, port })
         .catch(console.error)
@@ -27,66 +35,157 @@ function Subscribe(topic: String, periodic?: number, all?: boolean, prefix?: Str
         .catch(console.error)
 }
 
-// TODO: backend must be implemented
-function Publish(topic: String, message: String): void {
+/**
+ * Publishes a value to a topic
+ * @param topic the topic to publish to
+ * @param message the value to publish
+ *
+ * This function calls on the native backend and may result in a crash.
+ * TODO: backend must be implemented
+ */
+function Publish(topic: String, message: any): void {
     invoke('publish_topic', { topic, message })
         .catch(console.error)
 }
 
-// TODO: backend must be implemented
+/**
+ * Unsubscribes from a topic
+ * @param topic the topic to unsubscribe from
+ *
+ * This function calls on the native backend and may result in a crash.
+ * TODO: backend must be implemented
+ */
 function Unsubscribe(topic: String): void {
     invoke('unsubscribe_from_topic', { topic })
         .catch(console.error)
 }
 
-// TODO: backend must be implemented, consider swapping to type safe return
+/**
+ * Gets the value of a topic
+ * @param topic the topic to get the value of
+ * @return the value of the topic
+ *
+ * This function calls on the native backend and may result in a crash.
+ * TODO: backend must be implemented
+ */
 function GetEntry(topic: String): any {
     invoke('get_entry', { topic })
         .then((entry) => { return entry })
         .catch(console.error)
 }
 
-// TODO: backend must be implemented
+/**
+ * Sets the value of an integer topic
+ * @param topic the topic to set the value of
+ * @param value the integer value to set the topic to
+ *
+ * This function calls on the native backend and may result in a crash.
+ * TODO: backend must be implemented
+ */
 function SetInteger(topic: String, value: number): void {
     invoke('set_integer_entry', { topic, value })
         .catch(console.error)
 }
 
+/**
+ * Sets the value of an integer array topic
+ * @param topic the topic to set the value of
+ * @param value the integer array value to set the topic to
+ *
+ * This function calls on the native backend and may result in a crash.
+ * TODO: backend must be implemented
+ */
 function SetIntigerArray(topic: String, value: number[]): void {
     invoke('set_integer_arr_entry', { topic, value })
         .catch(console.error)
 }
 
+/**
+ * Sets the value of a floating point topic
+ * @param topic the topic to set the value of
+ * @param value the floating point value to set the topic to
+ *
+ * This function calls on the native backend and may result in a crash.
+ * TODO: backend must be implemented
+ */
 function SetFloat(topic: String, value: number): void {
     invoke('set_float_entry', { topic, value })
         .catch(console.error)
 }
 
+/**
+ * Sets the value of a floating point array topic
+ * @param topic the topic to set the value of
+ * @param value the floating point array value to set the topic to
+ *
+ * This function calls on the native backend and may result in a crash.
+ * TODO: backend must be implemented
+ */
 function SetFloatArray(topic: String, value: number[]): void {
     invoke('set_float_arr_entry', { topic, value })
         .catch(console.error)
 }
 
+/**
+ * Sets the value of a boolean topic
+ * @param topic the topic to set the value of
+ * @param value the boolean value to set the topic to
+ *
+ * This function calls on the native backend and may result in a crash.
+ * TODO: backend must be implemented
+ */
 function SetBoolean(topic: String, value: boolean): void {
     invoke('set_boolean_entry', { topic, value })
         .catch(console.error)
 }
 
+/**
+ * Sets the value of a boolean array topic
+ * @param topic the topic to set the value of
+ * @param value the boolean array value to set the topic to
+ *
+ * This function calls on the native backend and may result in a crash.
+ * TODO: backend must be implemented
+ */
 function SetBooleanArray(topic: String, value: boolean[]): void {
     invoke('set_boolean_arr_entry', { topic, value })
         .catch(console.error)
 }
 
+/**
+ * Sets the value of a byte array topic
+ * @param topic the topic to set the value of
+ * @param value the byte array value to set the topic to
+ *
+ * This function calls on the native backend and may result in a crash.
+ * TODO: backend must be implemented
+ */
 function SetByteArray(topic: String, value: any[]): void {
     invoke('set_byte_arr_entry', { topic, value })
         .catch(console.error)
 }
 
+/**
+ * Sets the value of a string topic
+ * @param topic the topic to set the value of
+ * @param value the string value to set the topic to
+ *
+ * This function calls on the native backend and may result in a crash.
+ * TODO: backend must be implemented
+ */
 function SetString(topic: String, value: String): void {
     invoke('set_string_entry', { topic, value })
         .catch(console.error)
 }
 
+/**
+ * Sets the value of a string array topic
+ * @param topic the topic to set the value of
+ * @param value the string array value to set the topic to
+ *
+ * This function calls on the native backend and may result in a crash.
+ * TODO: backend must be implemented
+ */
 function SetStringArray(topic: String, value: String[]): void {
     invoke('set_string_arr_entry', { topic, value })
         .catch(console.error)
