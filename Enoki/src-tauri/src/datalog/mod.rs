ae18421557efeaa9907eb8d47b07a0467c2444cd
oh-yes-0-fps <foryt.bowan@gmail.com>
2023-06-18 23:32:19 -0400

    whopsie daisies2

--- Enoki/src-tauri/src/datalog/mod.rs
+++ Enoki/src-tauri/src/datalog/mod.rs
@@ -12,7 +12,7 @@ use tauri_cmds::*;
 pub static DATALOG: Lazy<Arc<Mutex<DataLogDaemon>>> = Lazy::new(|| Arc::new(Mutex::new(handler::create_datalog_daemon())));
 
 pub fn datalog_plugin<R: tauri::Runtime>() -> tauri::plugin::TauriPlugin<R> {
-    tauri::plugin::Builder::new("nt")
+    tauri::plugin::Builder::new("datalog")
         .setup(|_app_handle| {tracing::info!("Setting up datalog plugin"); Ok(())})
         .invoke_handler(tauri::generate_handler![
             read_datalog,
