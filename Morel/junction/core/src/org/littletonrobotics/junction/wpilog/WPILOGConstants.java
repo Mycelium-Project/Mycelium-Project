c5e2aef58ff11b24549c79af87ff5e34b97a16cc
Autumn <62668093+Autumn-Ou@users.noreply.github.com>
2023-07-07 18:04:40 -0400

    Morel be gone

--- Morel/junction/core/src/org/littletonrobotics/junction/wpilog/WPILOGConstants.java
+++
@@ -1,6 +0,0 @@
-package org.littletonrobotics.junction.wpilog;
-
-class WPILOGConstants {
-    public static final String extraHeader = "AdvantageKit";
-    public static final String entryMetadata = "{\"source\":\"AdvantageKit\"}";
-}
